196840e4cd621f8baa42809b1c84a1725238cf92
Ashbel <kashbel747@gmail.com>
2025-08-21 15:44:32 +0300

    Feat: Add the frontend component of the lesson player

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import { Layout } from "./components/layouts/Layout";
 import Dashboard from "./pages/DashboardPage";
 import Home from "./pages/HomePage";
+import LessonPlayer from "./components/modules/LessonPlayer";
 
 function App() {
   return (
@@ -12,6 +13,32 @@ function App() {
         element={
           <Layout>
             <Home />
+          </Layout>
+        }
+      />
+
+      <Route
+        path="/lesson"
+        element={
+          <Layout>
+            <LessonPlayer
+              lesson={{
+                id: "1",
+                title: "Introduction to React",
+                videoUrl: "https://www.w3schools.com/html/mov_bbb.mp4",
+                notes: "This is a sample lesson with **markdown** notes.",
+                duration: 100
+              }}
+              progress={{
+                lessonId: "1",
+                completed: false,
+                progress: 0,
+                lastWatched: 0
+              }}
+              onProgressUpdate={(progress) => console.log("Progress updated:", progress)}
+              onNextLesson={() => console.log("Go to next lesson")}
+              onPrevLesson={() => console.log("Go to previous lesson")}
+            />
           </Layout>
         }
       />
