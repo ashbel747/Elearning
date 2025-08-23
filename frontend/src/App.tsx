0410684606967ade583c1a658457f1f9d738b9dc
Ashbel <kashbel747@gmail.com>
2025-08-23 08:56:23 +0300

    Fixed routing problem

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { Layout } from "./components/layouts/Layout";
 import Home from "./pages/HomePage";
 import LessonPlayer from "./components/modules/LessonPlayer";
@@ -16,87 +16,85 @@ function App() {
   return (
     <AuthProvider>
       <QuizProvider>
-        <Router>
-          <Routes>
-            {/* Auth routes */}
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/reset-password" element={<ChangePassword />} />
+        <Routes>
+          {/* Auth routes */}
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/reset-password" element={<ChangePassword />} />
 
-            {/* Protected Student Quiz */}
-            <Route
-              path="/quiz/:lessonId"
-              element={
-                <ProtectedRoute role="student">
-                  <Layout>
-                    <QuizPage />
-                  </Layout>
-                </ProtectedRoute>
-              }
-            />
-
-            {/* Instructor Quiz Management */}
-            <Route
-              path="/quiz"
-              element={
-                <ProtectedRoute role="instructor">
-                  <Layout>
-                    <CreateQuizPage />
-                  </Layout>
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/view-quiz/:lessonId"
-              element={
-                <ProtectedRoute role="instructor">
-                  <Layout>
-                    <ViewQuizPage />
-                  </Layout>
-                </ProtectedRoute>
-              }
-            />
-
-            {/* Lesson route */}
-            <Route
-              path="/lesson"
-              element={
+          {/* Protected Student Quiz */}
+          <Route
+            path="/quiz/:lessonId"
+            element={
+              <ProtectedRoute role="student">
                 <Layout>
-                  <LessonPlayer
-                    lesson={{
-                      id: "1",
-                      title: "Introduction to React",
-                      videoUrl: "https://www.w3schools.com/html/mov_bbb.mp4",
-                      notes: "This is a sample lesson with **markdown** notes.",
-                      duration: 100,
-                    }}
-                    progress={{
-                      lessonId: "1",
-                      completed: false,
-                      progress: 0,
-                      lastWatched: 0,
-                    }}
-                    onProgressUpdate={(progress) =>
-                      console.log("Progress updated:", progress)
-                    }
-                    onNextLesson={() => console.log("Go to next lesson")}
-                    onPrevLesson={() => console.log("Go to previous lesson")}
-                  />
+                  <QuizPage />
                 </Layout>
-              }
-            />
+              </ProtectedRoute>
+            }
+          />
 
-            {/* Public homepage */}
-            <Route
-              path="/"
-              element={
+          {/* Instructor Quiz Management */}
+          <Route
+            path="/quiz"
+            element={
+              <ProtectedRoute role="instructor">
                 <Layout>
-                  <Home />
+                  <CreateQuizPage />
                 </Layout>
-              }
-            />
-          </Routes>
-        </Router>
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/view-quiz/:lessonId"
+            element={
+              <ProtectedRoute role="instructor">
+                <Layout>
+                  <ViewQuizPage />
+                </Layout>
+              </ProtectedRoute>
+            }
+          />
+
+          {/* Lesson route */}
+          <Route
+            path="/lesson"
+            element={
+              <Layout>
+                <LessonPlayer
+                  lesson={{
+                    id: "1",
+                    title: "Introduction to React",
+                    videoUrl: "https://www.w3schools.com/html/mov_bbb.mp4",
+                    notes: "This is a sample lesson with **markdown** notes.",
+                    duration: 100,
+                  }}
+                  progress={{
+                    lessonId: "1",
+                    completed: false,
+                    progress: 0,
+                    lastWatched: 0,
+                  }}
+                  onProgressUpdate={(progress) =>
+                    console.log("Progress updated:", progress)
+                  }
+                  onNextLesson={() => console.log("Go to next lesson")}
+                  onPrevLesson={() => console.log("Go to previous lesson")}
+                />
+              </Layout>
+            }
+          />
+
+          {/* Public homepage */}
+          <Route
+            path="/"
+            element={
+              <Layout>
+                <Home />
+              </Layout>
+            }
+          />
+        </Routes>
       </QuizProvider>
     </AuthProvider>
   );
